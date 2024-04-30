079780cc844844bf3b0165de70134e95944ca1e0
ChiPhanThanh <67159074+ChiPhanThanh@users.noreply.github.com>
2024-04-30 22:33:01 +0700

    học thêm về ArrayList

--- chii/src/Sleep.java
+++
@@ -1,8 +0,0 @@
-public class Sleep {
-    public static void main(String [] args){
-        for ( int i =0; i < 10000000; i++){
-            System.out.print(" Go to Sleep + MLV mai dinh " + "\n");
-            System.out.print(" Quyen, Quyen Quyen, Quyen " + "\n");
-        }
-    }
-}
